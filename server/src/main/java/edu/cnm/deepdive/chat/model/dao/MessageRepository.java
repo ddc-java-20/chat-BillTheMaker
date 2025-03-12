18206fc0403ebf4d96c6202b0d7a13fe0cd0e6f0
Bill <wholcombe@cnm.edu>
2025-03-12 10:52:16 -0600

    server side long polling in progress

--- server/src/main/java/edu/cnm/deepdive/chat/model/dao/MessageRepository.java
+++ server/src/main/java/edu/cnm/deepdive/chat/model/dao/MessageRepository.java
@@ -5,12 +5,18 @@ import edu.cnm.deepdive.chat.model.entity.Message;
 import edu.cnm.deepdive.chat.model.entity.User;
 import java.time.Instant;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
   List<Message> getAllByChannelAndPostedAfterOrderByPostedAsc(Channel channel, Instant posted);
+
+
+  @Query(LATEST_POSTED_QUERY)
+  List<Instant> getLastPostedByChannelAndPostedAfter(
+      Channel channel, Instant posted, Pageable pageable);
 
 
 }
